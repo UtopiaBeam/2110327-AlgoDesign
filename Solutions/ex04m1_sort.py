32c5920ad056f13b5961c0fc08ca4b02cd97e42c
UtopiaBeam <utopiabeam@gmail.com>
2019-01-24 15:17:26 +0700

    Add solution

---
+++ Solutions/ex04m1_sort.py
@@ -0,0 +1,14 @@
+from sys import stdin as kb
+from collections import Counter
+
+
+def mn(x, y):
+    global dc
+    return min(dc[x, y], dc[y, x])
+
+
+n, ls = int(kb.readline()), list(map(int, kb.readline().split()))
+dc = Counter(zip(ls, sorted(ls)))
+q = n - (dc[1, 1] + dc[2, 2] + dc[3, 3])
+s = mn(1, 2) + mn(2, 3) + mn(3, 1)
+print((q+s) * 2//3 - s)
