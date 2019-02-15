50dc78fa9020b07a7e68a1bfe97e25c46edce3db
UtopiaBeam <utopiabeam@gmail.com>
2019-02-15 15:37:40 +0700

    Add keyword

--- Solutions/a59_midp4_mcsw.py
+++ Solutions/a59_midp4_mcsw.py
@@ -1,6 +1,6 @@
 #
 # AUTHOR: Krist Pornpairin
-# KEYWORD:
+# KEYWORD: Sliding window minimum
 #
 
 from itertools import accumulate
@@ -13,8 +13,10 @@ _, k = map(int, input().split())
 dq = deque([Data(0, 0)])
 mx = -1e9
 for i, qsm in enumerate(accumulate(map(int, input().split())), 1):
-    while dq[0].idx + k < i: dq.popleft()
+    while dq[0].idx + k < i:
+        dq.popleft()
     mx = max(mx, qsm - dq[0].val)
-    while dq and dq[-1].val > qsm: dq.pop()
+    while dq and dq[-1].val > qsm:
+        dq.pop()
     dq.append(Data(i, qsm))
 print(mx)
